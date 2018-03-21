a562df935bffa1a127b41a17615b67bee109d6a9
thanhnh <thanhnguyenhuu.cs@gmail.com>
2018-03-21 12:13:42 +0800

    Initial Commit

---
+++ cityu_map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cityu_map
+//
+//  Created by Alan on 3/21/18.
+//  Copyright © 2018 Alan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
